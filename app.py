bed3190846c3d1a5998b95c494a642b89d7b57b1
RHormann <rowanhormann@gmail.com>
2025-04-12 15:42:59 +0100

    app.py can now be ran and added routing for initial pages

--- app.py
+++ app.py
@@ -2,5 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 @app.route("/")
-def index():
+def landingPage():
     return "<p>Welcome to my page!<p>"
+
+@app.route("/signup")
+def signup():
+    pass
+
+@app.route("/signin")
+def signin():
+    pass
+
+@app.route("/dashboard")
+def dashboard():
+    pass
+
+if __name__ == "__main__":
+    app.run(debug=False)
