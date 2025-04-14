f1259170d0559a17673d8b20a213db5a3f49c8e2
RHormann <rowanhormann@gmail.com>
2025-04-14 17:09:04 +0100

    Sign up page now uses Dependancy injection to validate form

---
+++ src/services/ServiceFactory.py
@@ -0,0 +1,3 @@
+from src.services.auth_services.AuthService import AuthService
+
+auth_service = AuthService()
--- src/services/auth_services/AuthService.py
+++ src/services/auth_services/AuthService.py
@@ -7,56 +7,56 @@ class AuthService:
     Validates the signup form data
     Returns (is_valid, error_message)
     """
-    def __init__(self, signupform):
-        self.signupform = signupform
+    def __init__(self):
+        pass
 
 
-    def signup(self):
+    def signup(self, signupform):
         #outside files call this method and only this method
         #parse errors back through here
-        is_valid, errors = self.validateSignupform()
+        is_valid, errors = self.__validateSignupform(signupform)
         if not is_valid:
             return False, errors
         print('form is valid attempting to contact database...')
         #once implimented, this section here will use other layers to contact database
         return True, None
 
-    def validateSignupform(self):
+    def __validateSignupform(self, signupform):
         validationerrors = []
         is_valid = True
         #all boxes are not empty
-        if not self.signupform.get("Name"):
+        if not signupform.get("Name"):
             error = 'Username is required'
             validationerrors.append(error)
             is_valid = False
-        if not self.signupform.get("Email"):
+        if not signupform.get("Email"):
             error = 'Email is required'
             validationerrors.append(error)
             is_valid = False
-        if not self.signupform.get("Password"):
+        if not signupform.get("Password"):
             error = 'Password is required'
             validationerrors.append(error)
             is_valid = False
-        if not self.signupform.get("ConfirmPassword"):
+        if not signupform.get("ConfirmPassword"):
             error = 'Password confirmation is required'
             validationerrors.append(error)
             is_valid = False
 
         #email is in correct format
-        email = self.signupform.get("Email")
+        email = signupform.get("Email")
         if email.find("@") == -1 or email.find(".") == -1:
             error = 'Email must be a valid email address'
             validationerrors.append(error)
             is_valid = False
 
         #password is atleast 6 characters long and contains a capital letter
-        password = self.signupform.get("Password")
+        password = signupform.get("Password")
         if len(password) < 6:
             error = 'Password must be at least 6 characters'
             validationerrors.append(error)
             is_valid = False
         #password and confirm password are the same
-        if self.signupform.get("ConfirmPassword") != self.signupform.get("Password"):
+        if signupform.get("ConfirmPassword") != signupform.get("Password"):
             error = 'Passwords do not match'
             validationerrors.append(error)
             is_valid = False
