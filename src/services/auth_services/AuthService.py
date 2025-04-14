7348469c341d3cb1f0850010a5a68a00f05570a1
RHormann <rowanhormann@gmail.com>
2025-04-14 13:40:52 +0100

    Form validation apart from db checking done

--- src/services/auth_services/AuthService.py
+++ src/services/auth_services/AuthService.py
@@ -27,9 +27,27 @@ class AuthService:
             error = 'Password confirmation is required'
             validationerrors.append(error)
             return False, validationerrors
+
         #email is in correct format
+        email = signupform.get("Email")
+        if email.find("@") == -1 or email.find(".") == -1:
+            error = 'Email must be a valid email address'
+            validationerrors.append(error)
+            return False, validationerrors
+
         #run a check to see if email already exists in db
+
         #password is atleast 6 characters long and contains a capital letter
+        password = signupform.get("Password")
+        if len(password) < 6:
+            error = 'Password must be at least 6 characters'
+            validationerrors.append(error)
+            return False, validationerrors
         #password and confirm password are the same
+        if signupform.get("ConfirmPassword") != signupform.get("Password"):
+            error = 'Passwords do not match'
+            validationerrors.append(error)
+            return False, validationerrors
+
         print('form is valid sending you to the dashboard!')
         return True, None
