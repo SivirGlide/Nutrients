a90b4fcbf7ed13f0547f8d70d4cb78a455c6e90b
RHormann <rowanhormann@gmail.com>
2025-04-18 21:47:22 +0100

    changed footer to only show if user isnt logged in

--- src/Pages/data/Analysis.py
+++ src/Pages/data/Analysis.py
@@ -1,5 +1,5 @@
 from flask import render_template
 
-
+#Purple colour scheme
 def get_analysis():
     return render_template('analysis.html')
--- src/Pages/data/FoodSearch.py
+++ src/Pages/data/FoodSearch.py
@@ -1,5 +1,5 @@
 from flask import render_template
 
-
+#Green colour scheme
 def get_foods():
     return render_template('food-lookup.html')
