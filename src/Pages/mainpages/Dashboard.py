c8a8f709fa8bcc44b889aac266a6ffe6104f284f
RHormann <rowanhormann@gmail.com>
2025-04-18 22:52:37 +0100

    Updated landing page images

--- src/Pages/mainpages/Dashboard.py
+++ src/Pages/mainpages/Dashboard.py
@@ -1,11 +1,22 @@
 from flask import render_template, request, session, redirect
 
-
 def getDashboard(food_service):
     if not session.get('uuid'):
         return redirect('auth/signin')
 
+    #get the username and time of day
+    user = 'Arizona'
+    time = 'Evening'
+    #get all meals eaten today, or return null
+
+    #if meals list has values, get the basic nutritional info of those meals
+
+    #if meals list has values, get the advanced nutritional info of those meals
+
+    #if meals list has values, get the graph info of those meals
+
     if request.method == "GET":
-        return render_template('dashboard.html')
+        return render_template('dashboard.html', user=user,time=time)
+
     if request.method == "POST":
         return render_template('dashboard.html')
